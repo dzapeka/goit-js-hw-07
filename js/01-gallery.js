4f02feb09c3c835694730843e218b3284acf3e06
dzapeka <douson@gmail.com>
2023-07-19 23:07:41 +0200

    implement task 2 - SimpleLightbox library

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector('ul.gallery');
-let lightbox = null;
+let lightbox;
 
 gallery.addEventListener('click', selectImageHandler);
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('ul.gallery');
+
+createGallery();
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGallery() {
+  const galleryMarkup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+}
